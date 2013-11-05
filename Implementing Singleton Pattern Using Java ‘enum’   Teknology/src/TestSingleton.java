a4bc8e79b50f22747ccfc9a47965e81e37117ae8
ChrisWongAtCUHK <wc38.wong@gmail.com>
2013-11-05 16:24:32 +0800

    Writing a singleton using enum is very simple and effective without worrying too much on concurrency and other stuff

--- Implementing Singleton Pattern Using Java ‘enum’   Teknology/src/TestSingleton.java
+++ Implementing Singleton Pattern Using Java ‘enum’   Teknology/src/TestSingleton.java
@@ -1,5 +1,21 @@
-public class TestSingleton {
+/** Description of TestSingleton
+ *        @author Chris Wong
+ *        @version 1.0 Nov 05, 2013
+ */
+ public class TestSingleton {
 
+	/** Description of main()        <br/>
+	 *                6 tests:        <br/>
+	 *					<ol>
+	 *						<li>Old Singleton version 1: not thread safe</li>
+	 *						<li>Old Singleton version 2: creates the Singleton Object even before it is actually required</li>
+	 *						<li>Old Singleton with Synchronized getInstance() method test</li>
+	 *						<li>Double Checked Singleton test</li>
+	 *						<li>enum version 1</li>
+	 *						<li>enum version 2(‘It is completely thread safe’)</li>
+	 *					</ol>
+	 *        
+	 */
 	public static void main(String[] args) {
 		// Old Singleton version 1: not thread safe
 		OldSingletonFirst oldSingletonFirst = OldSingletonFirst.getInstance();
